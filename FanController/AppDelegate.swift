95578914a76bd665e4b2cea90c8f1a24ad008751
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-15 17:58:43 +0100

    Initial Commit

---
+++ FanController/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FanController
+//
+//  Created by Christopher Boyle on 15/07/2020.
+//  Copyright © 2020 Christopher Boyle. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FanController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FanController
+//
+//  Created by Christopher Boyle on 15/07/2020.
+//  Copyright © 2020 Christopher Boyle. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
