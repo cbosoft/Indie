629b3c7c011b61224221812dc642203f41de873d
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-16 17:19:00 +0100

    Remove extraneous comment

--- FanController/AppDelegate.swift
+++ FanController/AppDelegate.swift
@@ -40,7 +40,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
         SMCKit.close()
     }
     
