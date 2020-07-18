03c3275a728c5050c02c1abfee8c67e3163658ea
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-18 17:55:55 +0100

    Add Property object for storing simple SMC key data and display info

--- Indie/Property.swift
+++ Indie/Property.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+
+
+class Property {
+    
+    var keys: [String] = []
+    var units : String = ""
+    
+    enum errors : Error {
+        case keyLengthError
+    }
+    
+    init() {
+        // do nothing
+    }
+    
+    init(fromString key: String, units: String) throws {
+        
+        if key.count != 4 {
+            throw errors.keyLengthError
+        }
+        
+        self.keys = [key]
+        self.units = units
+    }
+    
+    init(fromArr keys: [String], units: String) throws {
+        
+        for key in keys {
+            if (key.count != 4) {
+                throw errors.keyLengthError
+            }
+        }
+        
+        self.keys = keys
+        self.units = units
+    
+    }
+    
+    func is_empty() -> Bool {
+        return self.keys.count == 0
+    }
+    
+    func measure() -> String {
+        var tot: Double = 0.0
+        var n: Int = 0
+        
+        for k in self.keys {
+            let val = SMCKit.easyReadData(k)
+            
+            // if val is not nan...
+            if val == val {
+                tot += val
+                n += 1
+            }
+        }
+        
+        let display: Double = n>0 ? tot/Double(n) : Double.nan
+        
+        return String(format: "%.1f%@", display, self.units)
+    }
+}
