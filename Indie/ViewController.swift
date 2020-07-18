452a13bc6a7a6e5082c75ad0f54af8089fbda4f3
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-18 12:13:55 +0100

    Add new UI elements to ViewController class

--- Indie/ViewController.swift
+++ Indie/ViewController.swift
@@ -9,15 +9,37 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    @IBOutlet weak var chk_temperature: NSButton!
-    @IBOutlet weak var chk_floating_setpoint: NSButton!
-    @IBOutlet weak var quit_button: NSButton!
+    
+    @IBOutlet weak var rad_p1_type: NSButton!
+    @IBOutlet weak var rad_p2_type: NSButton!
+    @IBOutlet weak var rad_p2_other: NSButton!
+    @IBOutlet weak var dd_p1_choose: NSPopUpButton!
+    @IBOutlet weak var dd_p2_choose: NSPopUpButton!
+    @IBOutlet weak var dd_p1_custom_type: NSPopUpButton!
+    @IBOutlet weak var dd_p2_custom_type: NSPopUpButton!
+    @IBOutlet weak var ent_p1_custom: NSTextField!
+    @IBOutlet weak var ent_p2_custom: NSTextField!
+    @IBOutlet weak var chk_p2_enabled: NSButton!
+    
+    // TODO: generate based on system
+    let predef_keys = [
+        "Fan 0 Speed": "F0Ac",
+        "CPU 0 Proximity Temp.": "TC0P",
+        "CPU 0 Av. Core Temp.": ("TC1C", "TC2C", "TC3C", "TC4C")
+        ] as [String : Any];
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // TODO load predef_keys based on system
+        
+        // Set predefined keys in drop downs
+        dd_p1_choose.removeAllItems();
+        dd_p2_choose.removeAllItems();
+        for kv in self.predef_keys {
+            dd_p1_choose.addItem(withTitle: kv.key)
+            dd_p2_choose.addItem(withTitle: kv.key)
+        }
     }
 
     override var representedObject: Any? {
@@ -30,11 +52,35 @@ class ViewController: NSViewController {
         exit(0)
     }
     
-    @IBAction func chk_temperature_toggled(_ sender: Any) {
-        self.chk_floating_setpoint.isEnabled = (self.chk_temperature.state == NSControl.StateValue.on)
+    @IBAction func chck_p1_changed(_ sender: Any) {
+        
+        // predicate on predefined: true if predefined key
+        let predef_p = self.rad_p1_type.state == NSControl.StateValue.on
+
+        self.dd_p1_choose.isEnabled = predef_p;
+        self.ent_p1_custom.isEnabled = !predef_p;
+        self.dd_p1_custom_type.isEnabled = !predef_p;
     }
     
-    @IBAction func chk_floating_toggled(_ sender: Any) {
+    @IBAction func chk_p2_changed(_ sender: Any) {
+        
+        // predicate on predefined: true if predefined key
+        let predef_p = self.rad_p2_type.state == NSControl.StateValue.on
+
+        self.dd_p2_choose.isEnabled = predef_p;
+        self.ent_p2_custom.isEnabled = !predef_p;
+        self.dd_p2_custom_type.isEnabled = !predef_p;
+    }
+    
+    @IBAction func chk_p2_enabled_changed(_ sender: Any) {
+        let enstate = self.chk_p2_enabled.state == NSControl.StateValue.on;
+        let tstate = self.rad_p2_type.state == NSControl.StateValue.on
+        
+        self.rad_p2_type.isEnabled = enstate
+        self.rad_p2_other.isEnabled = enstate
+        self.dd_p2_choose.isEnabled = enstate && tstate
+        self.ent_p2_custom.isEnabled = enstate && !tstate
+        self.dd_p2_custom_type.isEnabled = enstate && !tstate
     }
     
 }
